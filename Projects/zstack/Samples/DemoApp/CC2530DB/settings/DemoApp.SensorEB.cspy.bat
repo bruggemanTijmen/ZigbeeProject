3e195674c592cc49a54b1b3ebbd1188694346abc
bruggemanTijmen <tijmenb@ziggo.nl>
2016-03-23 20:19:21 +0100

    noob fix

--- Projects/zstack/Samples/DemoApp/CC2530DB/settings/DemoApp.SensorEB.cspy.bat
+++ Projects/zstack/Samples/DemoApp/CC2530DB/settings/DemoApp.SensorEB.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "C:\Users\Arnout\Documents\ZigbeeProject\Projects\zstack\Samples\DemoApp\CC2530DB\settings\DemoApp.SensorEB.general.xcl" --backend -f "C:\Users\Arnout\Documents\ZigbeeProject\Projects\zstack\Samples\DemoApp\CC2530DB\settings\DemoApp.SensorEB.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "C:\Users\Tijmen\Documents\ZigbeeProject\Projects\zstack\Samples\DemoApp\CC2530DB\settings\DemoApp.SensorEB.general.xcl" --backend -f "C:\Users\Tijmen\Documents\ZigbeeProject\Projects\zstack\Samples\DemoApp\CC2530DB\settings\DemoApp.SensorEB.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "C:\Users\Arnout\Documents\ZigbeeProject\Projects\zstack\Samples\DemoApp\CC2530DB\settings\DemoApp.SensorEB.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Arnout\Documents\ZigbeeProject\Projects\zstack\Samples\DemoApp\CC2530DB\settings\DemoApp.SensorEB.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "C:\Users\Tijmen\Documents\ZigbeeProject\Projects\zstack\Samples\DemoApp\CC2530DB\settings\DemoApp.SensorEB.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Tijmen\Documents\ZigbeeProject\Projects\zstack\Samples\DemoApp\CC2530DB\settings\DemoApp.SensorEB.driver.xcl" 
 
 @echo off 
 :end
